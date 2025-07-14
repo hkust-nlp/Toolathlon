26dddba8a90a414283ecc84ffd3906da8b4d3514
lockon-n <dd098309@126.com>
2025-07-14 23:59:55 +0800

    add task: finalpoolcn/find-alita-paper, add check on downloaded file; add support for english version

--- scripts/update_trajectory.sh
+++ scripts/update_trajectory.sh
@@ -1,13 +1,29 @@
 # 和scripts/debug_manual.sh基本一致，区别在于这里用到的scripts/model_wise下的配置文件会输出到 recorded_trajectories_v2, 随后会被上传
 
+task_dir=finalpoolcn/find-alita-paper
+
+# uv run demo.py \
+# --eval_config scripts/model_wise/eval_claude-4-sonnet.json \
+# --task_dir $task_dir \
+# --debug
+# # --multi_turn_mode
+
+# uv run demo.py \
+# --eval_config scripts/model_wise/eval_gpt-4.1-mini.json \
+# --task_dir $task_dir \
+# --debug
+# # --multi_turn_mode
+
 uv run demo.py \
 --eval_config scripts/model_wise/eval_claude-4-sonnet.json \
---task_dir finalpoolcn/find-alita-paper \
---debug
+--task_dir $task_dir \
+--debug \
+--en_mode
 # --multi_turn_mode
 
 uv run demo.py \
 --eval_config scripts/model_wise/eval_gpt-4.1-mini.json \
---task_dir finalpoolcn/find-alita-paper \
---debug
+--task_dir $task_dir \
+--debug \
+--en_mode
 # --multi_turn_mode
