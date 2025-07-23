0e99e628eddff4bebdc9ed246716888a8097a8ab
lockon-n <dd098309@126.com>
2025-07-23 10:52:40 +0800

    停止追踪 tasks/debug/debug-task 文件夹

--- tasks/debug/debug-task/token_key_session.py
+++
@@ -1,8 +0,0 @@
-from addict import Dict
-import os
-# this is the local token key session for local testing
-# will override the token key session in the global ones in configs/token_key_session.py
-all_token_key_session = Dict(
-    canvas_api_token = "7~9yRG84zETv9zFLXvRzB6EZnJmWkvethR3MEH3TJ6CfuA9nUr4GTBAJEkeGhJnYkV",
-    canvas_domain = "canvas.instructure.com",
-)
