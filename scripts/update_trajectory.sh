4442a847a74aa8dc225829e66373594087c3d06e
1421974407 <jp2022213320@qmul.ac.uk>
2025-07-28 23:46:43 +0800

    Update update_trajectory.sh

--- scripts/update_trajectory.sh
+++ scripts/update_trajectory.sh
@@ -1,6 +1,6 @@
 # 和scripts/debug_manual.sh基本一致，区别在于这里用到的scripts/model_wise下的配置文件会输出到 recorded_trajectories_v2, 随后会被上传
 
-task_dir=finalpool/excel-market-research
+task_dir=finalpool/dietary-health
 
 uv run demo.py \
 --eval_config scripts/model_wise/eval_claude-4-sonnet.json \
