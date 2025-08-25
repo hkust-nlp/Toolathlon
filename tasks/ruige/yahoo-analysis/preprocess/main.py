6ad739e7e05ca632847aa108999c426863141bb4
lockon-n <dd098309@126.com>
2025-08-26 01:51:32 +0800

    remove tasks/ruige

--- tasks/ruige/yahoo-analysis/preprocess/main.py
+++
@@ -1,6 +0,0 @@
-from utils.data_processing.process_ops import copy_multiple_times
-from argparse import ArgumentParser
-import os
-
-if __name__=="__main__":
-    print("Nothing to preprocess in this task.")
