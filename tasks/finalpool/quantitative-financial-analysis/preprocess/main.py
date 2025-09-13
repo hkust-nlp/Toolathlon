0bed38cb4f46ad5ee4ebf595e1dde91d85ac96a7
motigrez <grdyx2138039185@126.com>
2025-09-13 17:38:59 +0800

    add --agent_workspace

--- tasks/finalpool/quantitative-financial-analysis/preprocess/main.py
+++ tasks/finalpool/quantitative-financial-analysis/preprocess/main.py
@@ -295,6 +295,7 @@ def verify_cleanup():
 
 if __name__ == "__main__":
     parser = ArgumentParser()
+    parser.add_argument("--agent_workspace", required=True)
     parser.add_argument("--launch_time", required=False, help="Launch time")
     args = parser.parse_args()
     
