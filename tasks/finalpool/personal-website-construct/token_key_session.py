9f2f289325b93631c9c2f7a8ab484dafb5c3592e
Edric-Zhao <edriczhao2025@gmail.com>
2025-09-14 20:24:52 +0800

    rename a task

---
+++ tasks/finalpool/personal-website-construct/token_key_session.py
@@ -0,0 +1,23 @@
+from addict import Dict
+import os
+from pathlib import Path
+import json
+
+def get_github_token():
+    task_id = "personal_website_construct"
+    config_path = "configs/github_repos.json"
+    with open(config_path, 'r') as f:
+        data = json.load(f)
+    task = data.get(task_id)
+    if task:
+        return task.get("github_token", "")
+    else:
+        raise ValueError(f"[Warning] Task ID {task_id} not found in {config_path}.")
+
+github_token = get_github_token()
+if not github_token:
+    raise ValueError("[Warning] Task-Level GitHub token is not set.")
+else:
+    all_token_key_session = Dict(
+        github_token = github_token,
+    )
