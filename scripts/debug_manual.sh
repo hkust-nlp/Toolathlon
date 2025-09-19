f60cf467ed62e88d31e9ac20912a244da3e6c993
lockon-n <dd098309@126.com>
2025-09-20 02:58:33 +0800

    update

--- scripts/debug_manual.sh
+++ scripts/debug_manual.sh
@@ -7,10 +7,10 @@
 
 uv run demo.py \
 --eval_config scripts/model_wise/eval_claude-4-sonnet.json \
---task_dir  finalpool/k8s-redis-helm-upgrade \
+--task_dir  finalpool/email-paper-homepage \
 --debug \
---model_short_name grok-code-fast-1 \
+--model_short_name gpt-5 \
 --provider openrouter \
 --max_steps_under_single_turn_mode 200 \
---multi_turn_mode \
---manual \
+# --multi_turn_mode \
+# --manual \
